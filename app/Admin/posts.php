ddb48ff5248373e11c0729e5bef7ebd49ffe10db
Vytautas Dirzauskas <vytautas@kilo.lt>
2022-03-21 21:22:30 +0200

    fixes

--- app/Admin/posts.php
+++ app/Admin/posts.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * Redirect author pages to home
+ */
+add_action('template_redirect', 'growtype_template_redirect_author_pages');
+function growtype_template_redirect_author_pages()
+{
+    global $wp_query;
+
+    if (is_author()) {
+        wp_redirect(get_option('home'), 301);
+        exit;
+    }
+}
+
 /*
  * Change post elements
  */
