0d6d368e139b0e4f3d58d62271cbfec1464e1824
Vytautas Dirzauskas <vytautas@kilo.lt>
2022-04-10 16:34:26 +0300

    fixes

--- app/Plugins/woocommerce-simple-auctions/catalog/orderby.php
+++ app/Plugins/woocommerce-simple-auctions/catalog/orderby.php
@@ -6,7 +6,7 @@
 add_filter('woocommerce_default_catalog_orderby_options', 'growtype_woocommerce_simple_auction_default_catalog_orderby_options');
 function growtype_woocommerce_simple_auction_default_catalog_orderby_options($options)
 {
-    $options['ending_soonest'] = esc_html__('Sort auction by ending soonest', 'growtype');
+    $options['ending_soonest'] = esc_html__('Sort by ending soonest', 'growtype');
 
     return $options;
 }
