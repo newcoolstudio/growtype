1070db4ba4800c5caa04b5695055ed3294201663
Vytautas Dirzauskas <vytautas@kilo.lt>
2022-07-21 18:20:39 +0300

    fixes

--- resources/scripts/plugins/acf/sections/accordions/vertical.js
+++ resources/scripts/plugins/acf/sections/accordions/vertical.js
@@ -1,18 +1,17 @@
 function tabAccordionVertical() {
-  $('.b-accordion-vertical .b-accordion--tab').click(function () {
+    $('.b-accordion-vertical .b-accordion--tab').click(function () {
+        if (!$(this).hasClass('is-active')) {
+            let tab_nr = $(this).data('nr');
 
-    if(!$(this).hasClass('is-active')){
-      let tab_nr = $(this).data('nr');
-
-      $('.b-accordion-vertical .b-accordion--tab').removeClass('is-active');
-      $(this).addClass('is-active');
-
-      $('.b-accordion-vertical .b-accordion--content').slideUp().removeClass('is-active');
-
-      $('.b-accordion-vertical .b-accordion--content[data-nr="'+tab_nr+'"]').slideDown().addClass('is-active')
-    }
-
-  });
+            $('.b-accordion-vertical .b-accordion--tab').removeClass('is-active');
+            $(this).addClass('is-active');
+            $('.b-accordion-vertical .b-accordion--content').slideUp().removeClass('is-active');
+            $('.b-accordion-vertical .b-accordion--content[data-nr="' + tab_nr + '"]').slideDown().addClass('is-active')
+        } else {
+            $('.b-accordion-vertical .b-accordion--tab').removeClass('is-active');
+            $('.b-accordion-vertical .b-accordion--content').slideUp().removeClass('is-active');
+        }
+    });
 }
 
 export {tabAccordionVertical};
