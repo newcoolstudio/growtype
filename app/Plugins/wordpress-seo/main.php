4b3f38d5ead3d360be369c1d9028684e75c8b71f
Vytautas Dirzauskas <vytautas@kilo.lt>
2022-11-08 15:56:15 +0200

    qtranslate updates

--- app/Plugins/wordpress-seo/main.php
+++ app/Plugins/wordpress-seo/main.php
@@ -1,30 +1,5 @@
 <?php
 
-add_filter('wpseo_canonical', 'growtype_wpseo_change_canonical');
-function growtype_wpseo_change_canonical($url)
-{
-    global $post;
-
-    if (function_exists('qtranxf_getLanguage') && is_singular('post')) {
-        return get_permalink();
-    } else {
-        return $url;
-    }
-}
-
-/**
- * Qtranslate translate
- */
-add_filter('wpseo_title', 'growtype_wpseo_qtranslate_filter', 10, 1);
-add_filter('wpseo_metadesc', 'growtype_wpseo_qtranslate_filter', 10, 1);
-add_filter('wpseo_metakey', 'growtype_wpseo_qtranslate_filter', 10, 1);
-add_filter('wpseo_opengraph_title', 'growtype_wpseo_qtranslate_filter', 10, 1);
-add_filter('wpseo_opengraph_desc', 'growtype_wpseo_qtranslate_filter', 10, 1);
-function growtype_wpseo_qtranslate_filter($text)
-{
-    return __($text);
-}
-
 /**
  * Hide promo
  */
