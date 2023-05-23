e6e9935f26b49ded739e646d06774afe5f7c3d17
Vytautas Dirzauskas <vytautas@kilo.lt>
2023-05-23 15:50:46 +0300

    select updates

--- resources/scripts/plugins/chosen/select.js
+++ resources/scripts/plugins/chosen/select.js
@@ -1,18 +1,20 @@
 function select() {
     (function ($) {
-        window.select = $('select');
+        jQuery(document).ready(function () {
+            window.select = jQuery('select');
 
-        window.select.map(function (index, element) {
-            window.selectArgs = {
-                disable_search_threshold: 20,
-            };
+            window.select.map(function (index, element) {
+                window.selectArgs = {
+                    disable_search_threshold: 20,
+                };
 
-            if (!$(element).is(':visible')) {
-                window.selectArgs.width = '100%';
-            }
+                if (!jQuery(element).is(':visible')) {
+                    window.selectArgs.width = '100%';
+                }
 
-            $(element).chosen(window.selectArgs);
-        });
+                jQuery(element).chosen(window.selectArgs);
+            });
+        })
     })(jQuery);
 }
 
